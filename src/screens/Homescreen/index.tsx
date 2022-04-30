de8f607e1f44382d25949697c3753db64693401d
Enaturale <bukolaarowosebi@gmail.com>
2022-04-30 16:35:56 +0100

    Testing git

--- src/screens/Homescreen/index.tsx
+++ src/screens/Homescreen/index.tsx
@@ -4,8 +4,6 @@ import { View, StyleSheet, FlatList } from 'react-native';
 import ProductItem from '../../components/ProductItem/ProductItem';
 import products from '../../data/products';
 
-
-
 const HomeScreen = () => {
     return (
         <View style={styles.page}>
@@ -19,12 +17,7 @@ const HomeScreen = () => {
             <FlatList 
             data={products}
             renderItem={({item}) => <ProductItem item={item}/>}
-            />
-
-
-
-
-                            
+            />                            
         </View>
     )
 
